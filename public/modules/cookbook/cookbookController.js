d2f5e8da5da71b3733c252c8a51d77f9e14da807
Samantha Chow <samanthakaychow@gmail.com>
2015-12-07 21:15:02 -0800

    Dumb dynamic form field add/remove

--- public/modules/cookbook/cookbookController.js
+++ public/modules/cookbook/cookbookController.js
@@ -15,3 +15,16 @@ angular.module('app').controller('cookbookController', ['$scope', '$http', '$loc
         );
     
 }]);
+
+var rowNum = 0;
+function addRow(frm) {
+    rowNum++;
+    var row = '<p id="rowNum'+rowNum+'">Item quantity: <input type="text" name="qty[]" size="4" value="'+frm.add_qty.value+'"> Item name: <input type="text" name="name[]" value="'+frm.add_name.value+'"> <input type="button" value="Remove" onclick="rowRow('+rowNum+');"></p>';
+    jQuery('#itemRows').append(row);
+    frm.add_qty.value = '';
+    frm.add_name.value = '';
+}
+
+function removeRow(rnum) {
+    jQuery('#rowNum'+rnum).remove();
+}
